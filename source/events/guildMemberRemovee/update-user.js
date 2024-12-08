f58770258979ac724845bfed936ba1904190a746
Briar Chamberlin <izenthedev@gmail.com>
2024-12-08 10:09:48 -0500

    Update readme.md to include contact information; remove unused imports in update-user.js and ai.js.

--- source/events/guildMemberRemovee/update-user.js
+++ source/events/guildMemberRemovee/update-user.js
@@ -1,6 +1,5 @@
 const { updateLeftAt } = require("../../utils/leave-server");
 const handleError = require("../../utils/handle-error");
-const color = require("chalk");
 
 module.exports = async (client, user) => {
   try {
